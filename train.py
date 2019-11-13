655501d0412a3fce1de112f4eeb17d9fc37d4b54
Omar Wasfy <eng.omarwasfy@gmail.com>
2019-11-13 12:29:19 +0200

    Update train.py

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from tensorboardX import SummaryWriter
 MODEL_DIR = 'saved_models'
 NUM_EPOCHS = 1000
 
-def Propagate_MS(ms, model, F2, P2==[0.5, 0.75, 1.0]):
+def Propagate_MS(ms, model, F2, P2=[0.5, 0.75, 1.0]):
     h, w = F2.size()[2], F2.size()[3]
     
     msv_F2, msv_P2 = ToCudaVariable([F2, P2])
